e8d629d7a524dff20ea0bb4983799cfaf74b942c
hsalway1 <62750891+hsalway1@users.noreply.github.com>
2024-08-27 19:23:18 +0000

    added a test for addition of numbers

---
+++ factorial_of_number_test.py
@@ -0,0 +1,5 @@
+import add_numbers as an
+
+assert an.add_numbers(5, 2) == 7
+
+assert an.add_numbers(5, 4) == 6
--- main.py
+++
@@ -1,7 +0,0 @@
-def add_numbers(a, b):
-    return a + b
-
-num1 = 5
-num2 = 10
-result = add_number(num1, num2)  # Intentional error: function name is incorrect
-print("The sum is:", result)
