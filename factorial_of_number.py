ce5dfef08f458ad99a324e30ce4f5da27f3f2bb3
hsalway1 <62750891+hsalway1@users.noreply.github.com>
2024-09-08 18:52:21 +0000

    Made changes according to lint messages

--- factorial_of_number.py
+++ factorial_of_number.py
@@ -1,10 +1,18 @@
+"""
+This module contains a method for calculating the factorial of a number.
+"""
+
 def factorial(num1):
+    """
+    A method to get the factorial of a number.
+    """
     if num1 <= 1:
         return 1
     return factorial(num1 - 1) * num1
 
-num1 = 5
 
-fact = factorial(num1)
+NUM = 5
+
+fact = factorial(NUM)
 
 print("factorial of 5:", fact)
--- factorial_test.py
+++ factorial_test.py
@@ -1,7 +1,15 @@
 from factorial_of_number import factorial
 
 def test_factorial_of_five():
+    """
+    Testing the value of factorial for number 5.
+    """
     assert factorial(5) == 120
 
+
 def test_factorial_of_four():
+    """
+    Testing the value of factorial for number 10.
+    """
     assert factorial(4) == 24
+
