cfd62c0a53b1e62f1bb5d04540cc3f0f0ab37249
hsalway1 <62750891+hsalway1@users.noreply.github.com>
2024-09-09 20:11:22 +0000

    Uploading pylint trace to post_trace/pylint_trace.txt

--- merge_sort_test.py
+++ merge_sort_test.py
@@ -23,3 +23,9 @@ def test_five_elements():
     Test case for sorting five elements
     """
     assert merge_sort([6, 3, 20, 5, 2]) == [2, 3, 6, 20]
+
+def test_eight_elements():
+    """
+    Test case for sorting eight elements. It is deliberately kept failing.
+    """
+    assert merge_sort([8, 3, 9, 10, 3, 6, 1, 2]) == [1, 2, 3, 3, 6, 8, 9, 10]
