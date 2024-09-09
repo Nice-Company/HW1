07a7ecf1d2025648a6c3a5193b402f55d68a21c8
hsalway1 <62750891+hsalway1@users.noreply.github.com>
2024-09-09 23:45:20 +0000

    Added a failing test for homework 1

--- factorial_test.py
+++ factorial_test.py
@@ -17,3 +17,10 @@ def test_factorial_of_four():
     Testing the value of factorial for number 10.
     """
     assert factorial(4) == 24
+
+
+def test_factorial_of_zero():
+    """
+    Test case for factorial of zero. It is kept failing intentionally.
+    """
+    assert factorial(0) == 0
